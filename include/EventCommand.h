f6ec799a401aadba147ec523161dbe34a533a918
reeve <reeveswilde@gmail.com>
2025-12-17 14:02:35 -0700

    began an implementation of the Command design pattern, but realized I need to fix some lower level stuff before I can merge it with the rest of my code

---
+++ include/EventCommand.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
+#include <memory>
+#include <vector>
+
+#include "Board.h"
+
+struct UIInfo {
+  sf::RenderWindow& window;
+  Board& board;
+};
+
+class CommandDispatcher {
+ private:
+  std::vector<std::unique_ptr<IEventCommand>> commands;
+
+ public:
+  void add(std::unique_ptr<IEventCommand> command);
+  void dispatch(const sf::Event& event, UIInfo context);
+};
+
+class IEventCommand {
+ public:
+  virtual ~IEventCommand() = default;
+  virtual void execute(const sf::Event& event, UIInfo context) = 0;
+  virtual bool canExecute(const sf::Event& event, UIInfo context) = 0;
+};
+
+class CloseWindowCommand : public IEventCommand {
+ public:
+  void execute(const sf::Event& event, UIInfo context);
+  bool canExecute(const sf::Event& event, UIInfo context);
+};
+
+class PromoteCommand : public IEventCommand {
+ public:
+  void execute(const sf::Event& event, UIInfo context);
+  bool canExecute(const sf::Event& event, UIInfo context);
+};
+
+class DragAndDropCommand : public IEventCommand {
+ public:
+  void execute(const sf::Event& event, UIInfo context);
+  bool canExecute(const sf::Event& event, UIInfo context);
+};
---
+++ src/EventCommand.cpp
@@ -0,0 +1,57 @@
+#include "EventCommand.h"
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
+#include <vector>
+
+// Need to implement UIInfo into all commands, but I first need to create data
+// structures that store all relevant information that the commands need.
+// this includes: a way to know if a pawn is currently promoting, and know all
+// the info that dragAndDrop, findPiece, and dropPiece know. I want to cover
+// this with the struct UIInfo.
+
+void CommandDispatcher::add(std::unique_ptr<IEventCommand> command) {
+  // STUB
+  commands.push_back(std::move(command));
+  return;
+}
+
+void CommandDispatcher::dispatch(const sf::Event& event, UIInfo context) {
+  // STUB
+  for (auto& command : commands) {
+    if (command->canExecute(event, context)) {
+      command->execute(event, context);
+      return;
+    }
+  }
+}
+
+void CloseWindowCommand::execute(const sf::Event& event, UIInfo context) {
+  // close window
+  // STUB
+  return;
+}
+
+bool CloseWindowCommand::canExecute(const sf::Event& event, UIInfo context) {
+  return event.type == sf::Event::Closed;
+}
+
+void PromoteCommand::execute(const sf::Event& event, UIInfo context) {
+  // STUB
+  return;
+}
+
+bool PromoteCommand::canExecute(const sf::Event& event, UIInfo context) {
+  // STUB
+  return false;
+}
+
+void DragAndDropCommand::execute(const sf::Event& event, UIInfo context) {
+  // STUB
+  return;
+}
+
+bool DragAndDropCommand::canExecute(const sf::Event& event, UIInfo context) {
+  return event.type == sf::Event::MouseButtonPressed &&
+         event.mouseButton.button == sf::Mouse::Left;
+}
